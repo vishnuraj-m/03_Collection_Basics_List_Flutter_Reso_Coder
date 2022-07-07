bacc910f9b821d131023f3681e11a8d2ec3d9ff2
vishnuacappella <acappella.studiov@gmail.com>
2022-07-07 09:06:48 +0530

    dart file created

---
+++ bin/list.dart
@@ -0,0 +1,5 @@
+import 'package:list/list.dart' as list;
+
+void main(List<String> arguments) {
+  print('Hello world: ${list.calculate()}!');
+}
---
+++ test/list_test.dart
@@ -0,0 +1,8 @@
+import 'package:list/list.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
